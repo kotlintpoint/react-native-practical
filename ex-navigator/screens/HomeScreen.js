3acd9a4e7c067102a9ea9a4271dd687b7fa44593
Ankit Sodha - 9033310919 <ankit.sodha123@gmail.com>
2021-07-23 10:12:45 +0530

    React Native Navigator
    Install Dependency and Stack Navigator Intro

---
+++ ex-navigator/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingTop:100 }}>
+        <Text>Home Screen</Text>
+      </View>
+    )
+}
+
+export default HomeScreen
